373cf307a340a94d2ffd5083e31ec1cc036d1e44
trisha <144921673+trish404@users.noreply.github.com>
2026-01-02 13:06:32 +0530

    Create 1071-greatest-common-divisor-of-strings.py

---
+++ 1071-greatest-common-divisor-of-strings.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        len1 = len(str1)
+        len2 = len(str2)
+
+        def valid(k):
+            if len1 % k or len2 % k:
+                return False
+            base = str1[:k]
+            return str1 == base * (len1 // k) and str2 == base * (len2 // k)
+
+        for i in range(min(len1, len2), 0, -1):
+            if valid(i):
+                return str1[:i]
+
+        return ""
