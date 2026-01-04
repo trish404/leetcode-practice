8ea228947c66d30eb2fd027adc7e67ac79e00f70
trisha <144921673+trish404@users.noreply.github.com>
2026-01-04 21:30:32 +0530

    Add increasingTriplet method to Solution class
    Implement a solution to determine if an increasing triplet subsequence exists in a list of integers.

---
+++ 334-increasing-triplet-subsequence.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) < 3:
+            return False
+        first = float('inf')
+        second = float('inf')
+
+        for n in nums:
+            if n <= first:
+                first = n
+            elif n <= second:
+                second = n
+            else:
+                return True
+        return False 
