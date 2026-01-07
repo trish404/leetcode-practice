3328bf266264a2b698059c9fe84d812195f77569
trisha <144921673+trish404@users.noreply.github.com>
2026-01-07 10:44:57 +0530

    Implement reverseBits function to reverse bits of an integer

---
+++ 190-reverse-bits.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reverseBits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+        for _ in range(32):
+            res = (res << 1) | (n & 1)
+            n >>= 1
+        
+        return res
+        
