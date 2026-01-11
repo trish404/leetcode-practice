a62bf6f19282bbbac931af83c761cac1af6bda1d
trisha <144921673+trish404@users.noreply.github.com>
2026-01-11 14:59:33 +0530

    Refactor backtrack function for combination sum
    Refactor backtrack function to use target as a parameter and change the base case for appending results.

--- 38-combination-sum.py
+++ 38-combination-sum.py
@@ -8,18 +8,18 @@ class Solution(object):
         res = []
         candidates.sort()
 
-        def backtrack(start, curr, tot):
-            if tot == target:
-                res.append(list(curr))
-                return
-            if tot > target:
+        def backtrack(start, curr, target):
+            if target == 0:
+                res.append(curr)
                 return
             
             for i in range(start, len(candidates)):
-                num = candidates[i]
-                curr.append(num)
-                backtrack(i, curr, tot + num)
-                curr.pop()
-        backtrack(0, [], 0)
+                if candidates[i] > target:
+                    break
+                backtrack(i, curr + [candidates[i]], target - candidates[i])
+            
+            return 
+            
+        backtrack(0, [], target)
         return res
             
