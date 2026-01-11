6c0fd4f970ed344915114e124c6eaf8814654ad8
trisha <144921673+trish404@users.noreply.github.com>
2026-01-11 19:33:32 +0530

    Add minOperations method to calculate sum mod k
    Implement minOperations method to calculate minimum operations needed.

---
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minOperations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        s = sum(nums)
+
+        return s%k
+        
