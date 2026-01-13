9e5343deadcda813b0edcb35dacf794c999bfe64
trisha <144921673+trish404@users.noreply.github.com>
2026-01-13 20:14:38 +0530

    Create 1484-group-sold-products-by-the-date.py

---
+++ 1484-group-sold-products-by-the-date.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
+    return (
+        activities.groupby("sell_date")
+        .agg(
+            num_sold=("product", "nunique"),
+            products=("product", lambda x: ",".join(sorted(x.unique())))
+        ).reset_index()
+    )
