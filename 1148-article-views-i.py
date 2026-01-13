01ae41302d37ec5a4a13c2fc80461cd8a4448666
trisha <144921673+trish404@users.noreply.github.com>
2026-01-13 20:48:16 +0530

    Create 1148-article-views-i.py

---
+++ 1148-article-views-i.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def article_views(views: pd.DataFrame) -> pd.DataFrame:
+    return (
+        views[views["author_id"] == views["viewer_id"]][["author_id"]]
+        .drop_duplicates()
+        .rename(columns={"author_id": "id"})
+        .sort_values("id")
+    )
