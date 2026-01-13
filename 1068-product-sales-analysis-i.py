a6924239fb65a04df5c8981e82001523a9d537aa
trisha <144921673+trish404@users.noreply.github.com>
2026-01-13 19:34:04 +0530

    Create 1068-product-sales-analysis-i.py

---
+++ 1068-product-sales-analysis-i.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
+    return(
+        sales.merge(
+            product, on="product_id", how="left"
+        )[["product_name", "year", "price"]]
+    )
