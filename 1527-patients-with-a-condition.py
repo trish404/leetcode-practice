5b4be4423003d081c2a2114604e160ad2dba0822
trisha <144921673+trish404@users.noreply.github.com>
2026-01-14 10:32:23 +0530

    Create 1527-patients-with-a-condition.py

---
+++ 1527-patients-with-a-condition.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    return patients[
+        patients["conditions"].str.startswith("DIAB1") | 
+        patients["conditions"].str.contains(" DIAB1")
+    ]
