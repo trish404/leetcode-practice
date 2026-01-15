fdaa48f8a57ba1c75fdfe8c5d54b4c30edd08d26
trisha <144921673+trish404@users.noreply.github.com>
2026-01-15 10:55:32 +0530

    Create 1232-check-if-it-is-a-straight-line.py

---
+++ 1232-check-if-it-is-a-straight-line.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        x1, y1 = coordinates[0]
+        x2, y2 = coordinates[1]
+
+        dx = x2 - x1
+        dy = y2 - y1
+
+        for i in range(2, len(coordinates)):
+            x, y = coordinates[i]
+            if dy * (x - x1) != dx * (y - y1):
+                return False
+
+        return True
